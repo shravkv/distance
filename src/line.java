d6ba7daae431b3e097d1c70dbf1eb65db118f55b
sravan <sravan.k.vodnala@gmail.com>
2022-05-24 07:54:09 +0530

    Initial commit

---
+++ src/line.java
@@ -0,0 +1,12 @@
+public class line {
+    public static void main(string args[])
+    {
+          int x1,x2,y1,y2;
+         double dis;
+         x1=2;y1=3;x2=4;y2=1;
+         dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+         System.out.println("distancebetween"+("x1","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+
+    }
+
+}
